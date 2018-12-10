b36a382d078b3c701708854258d142d09b9ca367
Jquinn147 <40247932+Jquinn147@users.noreply.github.com>
2018-12-09 23:06:04 -0500

    Add files via upload
    ZombieBoy config Locator script.
    Will find and dump the config file for ZombieBoy.  To be used in conjuction with ZombieBoyDecode.py

---
+++ Python/ConfigLocator.py
@@ -0,0 +1,18 @@
+filename = input("Please enter the filename: --> ")
+
+with open(filename, 'rb') as f:
+	s = f.read()
+	soffset= s.find(b'\xFE\xFE\xFE\xFE\xFE\xFE\xFE\xFE\xFE\x43')
+	sConfigBuffer=soffset+9
+	#0044617461
+	eOffset = s.find(b'\x00\x44\x61\x74\x61')
+	eConfigBuffer=eOffset+5
+	print(sConfigBuffer)
+	print(eConfigBuffer)
+	tSize = eConfigBuffer - sConfigBuffer
+	print(tSize)
+	buf = buffer(s,sConfigBuffer,tSize)
+	print(buf)
+	of = open('outfile.txt', 'wb')
+	of.write(buf)
+	of.close()
